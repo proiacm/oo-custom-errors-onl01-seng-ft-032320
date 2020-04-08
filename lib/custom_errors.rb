2372ffc7f9888bfd7ce7db26d300f963ba201e2f
CiaraMaria Proia <proiacm@gmail.com>
2020-04-08 20:43:56 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,15 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+      if person.class != Person 
+        raise PartnerError
+      else
+      person.partner = self
+    end
   end
+  
+class PartnerError < StandardError
+end
 
 end
 
@@ -16,8 +23,6 @@ beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
-class PartnerError < StandardError
-end
 
 
 
